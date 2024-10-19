8cbb07bcc151580ff3f2f19c6877a2817d733727
ashar <ashar.kamal98@gmail.com>
2024-10-19 17:40:36 +0200

    add period finding algorithm

--- utils.py
+++ utils.py
@@ -1,4 +1,20 @@
 def bin_to_int(x):
-    """ Helper function """
+    """ Helper function to turn binary number to deciaml """
     my_int = int(x, base=2)
+    return my_int
+
+
+def int_to_bin(x,n):
+    """ Helper function to turn decimal to binary number containing n digits """
+    bn = bin(x)[2:].zfill(n)
+    return bn
+        
+
+def int_to_int(x,n):
+    """ Add description """
+    bn = bin(x)[2:]
+    my_int = 0
+    for (i,bit) in enumerate(bn[::-1]):
+        if bit == "1":
+            my_int += 2**(n+i)
     return my_int
